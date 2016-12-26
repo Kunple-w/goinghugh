8e760c810aa10a24af91d002c069b9d8789e646f
wangyongxu <wangyx1122@gmail.com>
2016-12-26 21:26:40 +0800

    工程正常运行。

--- goinghugh/settings.py
+++ goinghugh/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'goinghugh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR,'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -126,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/upload/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'upload').replace("\\",'/')
